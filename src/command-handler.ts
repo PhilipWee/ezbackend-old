0e252f73da890702420fa677687837eca01384a6
Philip Wee <philip.andrewweedewang@gmail.com>
2021-07-31 15:49:35 +0800

    Added initiate file
    For running initialization of files

--- src/command-handler.ts
+++ src/command-handler.ts
@@ -2,6 +2,7 @@ import {Command} from 'commander'
 import {readPackageUpSync} from 'read-pkg-up'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import initiate from './initiate.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,12 +15,6 @@ program
     .command('init')
     .description('Initialize ezbackend into your project.')
     .option('-f --force', 'Force add ezbackend')
-    .action((options) => initiate(options))
-
-
-
-const initiate = (options:any) => {
-    console.log(pkg)
-}
+    .action((options) => initiate(options,pkg))
 
 program.parse(process.argv)
---
+++ src/initiate.ts
@@ -0,0 +1,5 @@
+const logger = console
+
+export default function initiate(options:any,pkg:any) {
+   logger.log('hello world', pkg) 
+}  
