8daf061cfaa776c7fb5ebb0fbd61440fddb79cbf
Philip Wee <philip.andrewweedewang@gmail.com>
2021-07-31 16:16:23 +0800

    Starting point for not allowing directory imports

--- src/initiate.ts
+++ src/initiate.ts
@@ -1,8 +1,26 @@
-import chalk from 'chalk'
+import chalk from "chalk";
+import { ProjectType } from "./project-types.js";
+import { isEzbInstalled } from "./detect.js";
+import { readPackageJson } from "./js-package-manager";
 
-const logger = console
+const logger = console;
 
-export default function initiate(options:any,pkg:any) {
-    const welcomeMessage = 'EzBackend - An extensible backend optimised for the developer experience'
-    logger.log(chalk.inverse(`\n ${welcomeMessage} \n`))
-}  
+export default function initiate(options: any, pkg: any) {
+  const welcomeMessage =
+    "EzBackend - An extensible backend optimised for the developer experience";
+  logger.log(chalk.inverse(`\n ${welcomeMessage} \n`));
+
+  let projectType;
+
+  const ezbInstalled = isEzbInstalled(readPackageJson(), options.force);
+  projectType = ezbInstalled
+    ? ProjectType.ALREADY_HAS_EZB
+    : ProjectType.DEFAULT;
+  logger.log(projectType);
+
+  return installEzb(projectType);
+}
+
+const installEzb = (projectType:ProjectType) => {
+    console.log('Installing')
+}
