124d657fe59e62e3f9ce924cefe18bc439a20bd4
Philip Wee <philip.andrewweedewang@gmail.com>
2021-07-31 14:19:11 +0800

    Added command handler for init command

--- src/command-handler.ts
+++ src/command-handler.ts
@@ -1,1 +1,17 @@
-console.log('hello world')
+import {Command} from 'commander'
+
+const program = new Command()
+
+program
+    .command('init')
+    .description('Initialize ezbackend into your project.')
+    .option('-f --force', 'Force add ezbackend')
+    .action((options) => initiate(options))
+
+
+
+const initiate = (options:any) => {
+    console.log('hello world')
+}
+
+program.parse(process.argv)
