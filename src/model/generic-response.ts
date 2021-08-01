0c6d5fc3e3cc93cc6d078ca93ebe0b54b990e54b
Philip Wee <philip.andrewweedewang@gmail.com>
2021-08-01 22:07:34 +0800

    Working commit
    With create one api

---
+++ src/model/generic-response.ts
@@ -0,0 +1,6 @@
+export const badRequest = {
+    //TODO: Figure out how to make this 400 in swagger
+    statusCode: {type: 'number'},
+    error: {type:'string'},
+    message: {type:'string'}
+}
--- src/model/index.ts
+++ src/model/index.ts
@@ -1,107 +1,112 @@
 import { ModelAttributes, Model, ModelCtor } from "sequelize";
 
-//Testing stuff
-import { Sequelize } from "sequelize";
-import fastify, { RouteOptions, FastifyPlugin, FastifyRegisterOptions } from "fastify";
+import fastify, {
+  RouteOptions,
+  FastifyPlugin,
+  FastifyRegisterOptions,
+} from "fastify";
 import fastifySwagger from "fastify-swagger";
-//@ts-ignore
-import sjs from 'sequelize-json-schema' 
+import { getModelSchema } from "./sequelize-json-schema/index.js";
+import { badRequest } from "./generic-response.js";
+import pkg, { Sequelize } from "sequelize";
+//TODO: Properly set up the compiler so we don't have disgusting shit like this
+const { DataTypes } = pkg;
 
-const logger = console
+const logger = console;
 
 export class EzRouter {
-  routePrefix: string
-  routes:Array<RouteOptions>
+  routePrefix: string;
+  routes: Array<RouteOptions>;
 
   //TODO: Validation to ensure routeprefix is legitimate
   constructor(routePrefix: string) {
-    this.routePrefix = routePrefix
-    this.routes = []
+    this.routePrefix = routePrefix;
+    this.routes = [];
   }
 
-  public registerRoute(newRoute:RouteOptions) {
-    this.routes.push(newRoute)
+  public registerRoute(newRoute: RouteOptions) {
+    this.routes.push(newRoute);
   }
-  
+
   //TODO: Figure out how to add types to this
   public registerFunction() {
-    let that = this
-    return (function (server:any, opts:any, done:any) {
+    let that = this;
+    return function (server: any, opts: any, done: any) {
       that.routes.forEach((route) => {
-        server.route(route)
-      })
-      done()
-    })
+        server.route(route);
+      });
+      done();
+    };
   }
 }
 
-export class EzModel extends EzRouter{
-  
-  model: ModelCtor<Model<any,any>> | undefined
+export class EzModel extends EzRouter {
+  model: ModelCtor<Model<any, any>> | undefined;
   modelName: string;
   attributes: ModelAttributes<Model<any, any>>;
 
   //TODO: Validation to ensure modelName will not mess up the route prefix
   //Perhaps can use the inflection pluralisation library
   constructor(modelName: string, attributes: ModelAttributes<Model<any, any>>) {
-    super(`/${modelName}`) //Note: No trailing slash is intentional
+    super(`/${modelName}`); //Note: No trailing slash is intentional
     this.modelName = modelName;
     this.attributes = attributes;
   }
 
-  setModel(sequelize:Sequelize) {
-    this.model = sequelize.define(this.modelName,this.attributes)
+  setModel(sequelize: Sequelize) {
+    this.model = sequelize.define(this.modelName, this.attributes);
   }
 
-  getJsonSchema() {
+  getJsonSchema(full: boolean) {
     if (this.model === undefined) {
       //TODO: Custom error?
-      throw "Model has not been set yet"
+      throw "Model has not been set yet";
     }
 
-    return sjs.getModelSchema(this.model)
+    if (full) {
+      return getModelSchema(this.model);
+    } else {
+      return getModelSchema(this.model, {
+        exclude: ["id", "createdAt", "updatedAt"],
+      });
+    }
   }
 
-  init(sequelize:Sequelize) {
-    this.setModel(sequelize)
-    const apis = this.getAllAPIs()
+  init(sequelize: Sequelize) {
+    this.setModel(sequelize);
+    const apis = this.getAllAPIs();
     apis.forEach((api) => {
-      this.registerRoute(api)
-    })
+      this.registerRoute(api);
+    });
   }
 
   getAllAPIs() {
-    return [
-      this.createOneAPI()
-    ]
+    return [this.createOneAPI()];
   }
 
   createOneAPI() {
-
-    console.log(this.getJsonSchema())
-
-    const routeDetails:RouteOptions = {
+    let that = this;
+    const routeDetails: RouteOptions = {
       method: "POST",
-      url: '/',
+      url: "/",
       schema: {
-        body: this.getJsonSchema()
-        ,
+        body: this.getJsonSchema(false),
         response: {
-          200: {
-            type: 'object',
-            properties: {
-              hello: {type: 'string'}
-            }
-          }
-        }
+          200: this.getJsonSchema(true),
+          400: badRequest,
+        },
       },
-      handler(req,res) {
-        
-      }
-    }
-    return routeDetails
+      async handler(req, res) {
+        if (that.model === undefined) {
+          //TODO: Custom error?
+          throw "Model has not been set yet";
+        }
+        const newObj = await that.model.create(req.body);
+        res.send(newObj)
+      },
+    };
+    return routeDetails;
   }
-
 }
 
 export async function test() {
@@ -119,15 +124,24 @@ export async function test() {
         url: "https://github.com/Collaboroo/ezbackend",
         description: "Find more info here",
       },
-      host: "localhost",
+      // host: "localhost",
       schemes: ["http"],
       consumes: ["application/json"],
       produces: ["application/json"],
     },
   });
-  const sequelize = new Sequelize('sqlite::memory')
+  const sequelize = new Sequelize("sqlite::memory");
   //Imaginary end user code
-  const ezmodel = new EzModel("Users", {});
+  const ezmodel = new EzModel("Users", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
   ezmodel.registerRoute({
     method: "GET",
     url: `/:id`,
@@ -135,12 +149,13 @@ export async function test() {
     handler(request: any, reply: any) {
       reply.send(request.query.field);
     },
-  })
+  });
   //Imaginary end user code
-  ezmodel.init(sequelize)
-  server.register(ezmodel.registerFunction(), {prefix: ezmodel.routePrefix})
-  const port = 8000
+  ezmodel.init(sequelize);
+  server.register(ezmodel.registerFunction(), { prefix: ezmodel.routePrefix });
+  sequelize.sync()
+  const port = 8000;
   await server.listen(port);
   server.swagger();
-  logger.log(`EzBackend Listening on ${port}`)
+  logger.log(`EzBackend Listening on ${port}`);
 }
