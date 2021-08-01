116cd9fdebce1c5498b3815aba1fdd631ff66b19
Philip Wee <philip.andrewweedewang@gmail.com>
2021-08-01 20:37:28 +0800

    Just broken
    Not accepting int32

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,9 +1,11 @@
-import { ModelAttributes, Model } from "sequelize";
+import { ModelAttributes, Model, ModelCtor } from "sequelize";
 
 //Testing stuff
 import { Sequelize } from "sequelize";
 import fastify, { RouteOptions, FastifyPlugin, FastifyRegisterOptions } from "fastify";
 import fastifySwagger from "fastify-swagger";
+//@ts-ignore
+import sjs from 'sequelize-json-schema' 
 
 const logger = console
 
@@ -34,29 +36,56 @@ export class EzRouter {
 }
 
 export class EzModel extends EzRouter{
+  
+  model: ModelCtor<Model<any,any>> | undefined
   modelName: string;
   attributes: ModelAttributes<Model<any, any>>;
 
   //TODO: Validation to ensure modelName will not mess up the route prefix
+  //Perhaps can use the inflection pluralisation library
   constructor(modelName: string, attributes: ModelAttributes<Model<any, any>>) {
     super(`/${modelName}`) //Note: No trailing slash is intentional
     this.modelName = modelName;
     this.attributes = attributes;
-    this.registerRoute(this.createOneAPI())
+  }
+
+  setModel(sequelize:Sequelize) {
+    this.model = sequelize.define(this.modelName,this.attributes)
+  }
+
+  getJsonSchema() {
+    if (this.model === undefined) {
+      //TODO: Custom error?
+      throw "Model has not been set yet"
+    }
+
+    return sjs.getModelSchema(this.model)
+  }
+
+  init(sequelize:Sequelize) {
+    this.setModel(sequelize)
+    const apis = this.getAllAPIs()
+    apis.forEach((api) => {
+      this.registerRoute(api)
+    })
+  }
+
+  getAllAPIs() {
+    return [
+      this.createOneAPI()
+    ]
   }
 
   createOneAPI() {
+
+    console.log(this.getJsonSchema())
+
     const routeDetails:RouteOptions = {
       method: "POST",
       url: '/',
       schema: {
-        body:{
-          type: 'object',
-          properties: {
-            key1: {type: 'string'},
-            key2: {type: 'number'}
-          }
-        },
+        body: this.getJsonSchema()
+        ,
         response: {
           200: {
             type: 'object',
@@ -96,8 +125,8 @@ export async function test() {
       produces: ["application/json"],
     },
   });
-  
-  
+  const sequelize = new Sequelize('sqlite::memory')
+  //Imaginary end user code
   const ezmodel = new EzModel("Users", {});
   ezmodel.registerRoute({
     method: "GET",
@@ -107,6 +136,8 @@ export async function test() {
       reply.send(request.query.field);
     },
   })
+  //Imaginary end user code
+  ezmodel.init(sequelize)
   server.register(ezmodel.registerFunction(), {prefix: ezmodel.routePrefix})
   const port = 8000
   await server.listen(port);
